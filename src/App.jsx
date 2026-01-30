134a474e1b36927fac97d020aba995ee5be5717a
Azim.404 | Sofiane K <Sb.kherarfa.@gmail.com>
2026-01-30 15:02:59 +0100

    Force cache busting with version attribute

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen" data-version="2.0">
       <Header />
       <Hero />
       <About />
